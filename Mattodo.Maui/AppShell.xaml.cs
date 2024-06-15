34173d01f023d4b9e80062712c2e33e81b31de21
Matt Strong <swevo24@gmail.com>
2024-06-15 16:41:23 -0500

    default app

---
+++ Mattodo.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mattodo.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mattodo.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mattodo.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
